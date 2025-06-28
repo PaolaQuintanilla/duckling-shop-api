92d2f2f380e53c7f14f3a6dcb52f4e87c28e46e0
PaolaQuintanilla <74014.linares@gmail.com>
2025-06-27 21:35:18 -0400

    changes main file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
@@ -13,6 +13,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(3000);
+  await app.init();
 }
 bootstrap();
