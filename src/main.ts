2c5f551d84ff37e7394fec980ade9c06ae51a1d5
PaolaQuintanilla <74014.linares@gmail.com>
2025-09-10 08:51:17 -0400

    add origin cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import express from 'express';
+import * as bodyParser from 'body-parser';
 import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express();
@@ -10,10 +11,15 @@ let isInitialized = false;
 
 export async function createNestServer(): Promise<express.Express> {
   if (!isInitialized) {
+    server.use(bodyParser.json());
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
     app.enableCors({
-      origin: ['https://duckling-shop-client.vercel.app'],
+      origin: [
+        'https://duckling-shop-client.vercel.app',
+        'http://localhost:3001',
+        'https://duckling-shop-client-git-develop-paolas-projects-a3e71503.vercel.app',
+      ],
       allowedHeaders: ['Content-Type'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
